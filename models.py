6f56c42893c5c35ae91661eac4572ca07d75f1c9
JackFogerson <40747086+JackFogerson@users.noreply.github.com>
2026-07-12 03:26:35 -0700

    Add display name and life details to AncestorNode

--- models.py
+++ models.py
@@ -1,8 +1,12 @@
 # models.py
 
 class AncestorNode:
-    def __init__(self, name, base_ethnicities=None, father=None, mother=None):
+    def __init__(self, name, base_ethnicities=None, father=None, mother=None, display_name="", birth_year="", death_year="", is_living=False):
         self.name = name
+        self.display_name = display_name if display_name else name
+        self.birth_year = birth_year
+        self.death_year = death_year
+        self.is_living = is_living
         self.base_ethnicities = base_ethnicities if base_ethnicities else []
         self.father = father
         self.mother = mother
